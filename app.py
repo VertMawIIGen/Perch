5a269f6b0969e18ce0e56518a2bf8c120d81e5f0
Vert_MawIIGen <wijayaarchie@gmail.com>
2023-12-01 13:54:29 +1100

    Initial commit.

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")    #home page / first thing the user sees
+@app.route("/")  # home page / first thing the user sees
 def hello_world():
     return 'Hello World!'
 
-@app.route("/timetable") #the timetable section once you log in
+
+@app.route("/timetable")  # the timetable section once you log in
 def hello_world():
     return 'Hello World!'
 
-@app.route("/notices") #placeholder for future development
+
+@app.route("/notices")  # placeholder for future development
 def hello_world():
     return None
-
 
 
 if __name__ == '__main__':
