a53ecc199eca4f5ada5b478058f2e6ee16549ce0
Vert_MawIIGen <wijayaarchie@gmail.com>
2025-11-03 23:28:33 +1100

    Major Restarting Works
    Done:
    - Worked on the landing_page.html.
    - Did some restructuring to the files.
    - Made login work.
    
    Todo:
    - Make the landing page look better.
    - Create the logic itself for the timetable application.

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request #todo: implement database system for user prefs
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash, send_file, make_response
+from flask_oidc import OpenIDConnect
+from config import Config
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+oidc = OpenIDConnect(app)
+csrf = CSRFProtect(app)
 
 
-@app.route("/")  # home page / first thing the user sees
-def homepage(): #todo: login function
-    return render_template("homepage.html")
+# Login page redirecting to the school.
+@app.route('/login')
+@oidc.require_login
+def login():
+    return redirect(url_for("daily_timetable"))
+
+
+# landing page / first thing the user sees
+@app.route("/")
+def landing_page():
+    if oidc.user_loggedin:
+        flash("Successfully logged in.", "success")
+        return redirect(url_for("daily_timetable"))
+    else:
+        return render_template("landing_page.html")
 
 
 @app.route("/treetop")  # the timetable section once you log in
