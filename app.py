e8feaffbd052dc9875536a0c27b86002a60470c1
Vert_MawIIGen <wijayaarchie@gmail.com>
2025-11-04 14:15:10 +1100

    Getting Authenticated API Information
    Changes:
    - Added code to get information from the school authenticated API, using the user's access token.
    - Removed placeholder code for nest, added request code to get the daily notices for the day.
    
    Todo:
    - Code the front end, in order to make use of the information being obtained in the back end.
    - Account for when the school API returns unexpected values (failing gracefully).

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, jsonify, session, redirect, u
 from flask_oidc import OpenIDConnect
 from config import Config
 from flask_wtf.csrf import CSRFProtect
+import pprint
+import json
+import requests
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +23,7 @@ def login():
 
 # landing page / first thing the user sees
 @app.route("/")
+@oidc.require_login
 def landing_page():
     if oidc.user_loggedin:
         flash("Successfully logged in.", "success")
@@ -28,13 +33,50 @@ def landing_page():
 
 
 @app.route("/treetop")  # the timetable section once you log in
+@oidc.require_login
 def daily_timetable():
-    return render_template("daily_timetable.html")
+    # attempting to get authenticated api information
+    # using the requests library
+    access_token = session['oidc_auth_token'].get('access_token')
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get('https://student.sbhs.net.au/api/timetable/daytimetable.json', headers=headers)
+    json_data = response.json()
+
+    # pprint.pprint(json_data)
+
+    daily_bells = json_data.get('bells')
+    room_variations = json_data.get('roomVariations')
+    teacher_variations = json_data.get('classVariations')
+    student_timetable = json_data.get('timetable')
+
+    return render_template("daily_timetable.html", daily_bells=daily_bells, room_variations=room_variations,
+                           teacher_variations=teacher_variations, student_timetable=student_timetable)
 
 
-@app.route("/nest")  # placeholder for future development probably the daily notices
-def something():
-    return None
+@app.route("/nest")
+@oidc.require_login
+def daily_notices():
+    access_token = session['oidc_auth_token'].get('access_token')
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.get('https://student.sbhs.net.au/api/dailynews/list.json', headers=headers)
+    json_data = response.json()
+    notices = json_data.get('notices')
+    return render_template('daily_notices.html', notices=notices)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('landing_page'))
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
